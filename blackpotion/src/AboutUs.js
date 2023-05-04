f067b08e61445de6d4d6fd7e9858c44599fd20f1
Campbell20 <jcampbell145@alamo.edu>
2023-05-04 13:53:26 -0500

    Cleaning up the about us page

--- blackpotion/src/AboutUs.js
+++ blackpotion/src/AboutUs.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect } from 'react';
+
+
 
 function AboutUs() {
+  //adds google api inegration
+  useEffect(() => {
+    const googleMapScript = document.createElement('script');
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyApRiPhDF7UNsL1A4w1AvgzwAxLadfI1-I&callback=initMap`;
+    googleMapScript.async = true;
+    window.document.body.appendChild(googleMapScript);
+
+    googleMapScript.addEventListener('load', () => {
+      const mapOptions = {
+        center: { lat: 29.4877755, lng: -98.5522303 },
+        zoom: 15,
+        mapTypeId: 'roadmap',
+      };
+      const map = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+
+      const marker = new window.google.maps.Marker({
+        position: { lat: 29.4877755, lng: -98.5522303 },
+        map: map,
+        title: 'The Black Potion',
+      });
+      marker.setMap(map);
+    });
+  }, []);
   return (
-    <div class="container mt-5">
+
+    //ABOUT US HTML
+    <div class="container-xl mt-5">
       <h1 class="text-center mb-4">About The Black Potion</h1>
       <div class="row">
-        <div class="col-md-6">
-          <p>Located at 1900 Fredericksburg Rd Suite 101 in San Antonio, Texas, The Black Potion is a unique and exciting bar that offers a wide range of cocktails and drinks to suit all tastes.</p>
-
+        <div class="col-md-6 mb-5">
+          <p>Located at 1900 Fredericksburg Rd Suite 101 in San Antonio, Texas, The Black Potion is a unique and exciting bar that offers a wide range of cocktails and drinks to suit all tastes. In addition to being a great place to grab a drink, The Black Potion is also a hub for board game enthusiasts. With a diverse selection of games and a welcoming atmosphere, our bar is the perfect destination for a night of fun with friends or family.</p>
           <p>Our goal is to create a welcoming and friendly atmosphere where our customers can relax, have fun, and enjoy a great night out. Whether you're looking for a place to hang out with friends, catch up on work, or enjoy a date night, The Black Potion is the perfect destination.</p>
+          <div id="map" style={{ height: "400px" }}></div>
         </div>
 
 
         <div class="col-md-6">
           <h3>Contact Information</h3>
 
-          <div>
+          <div class="mb-3">
             <strong>The Black Potion</strong><br />
             1900 Fredericksburg Rd Suite 101<br />
             San Antonio, TX, United States, Texas<br />
